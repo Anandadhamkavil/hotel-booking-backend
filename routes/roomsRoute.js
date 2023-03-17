031cfa7e480e0bca6543455ed0f95ca67662baf0
Anandadhamkavil <anandadhamkavil123@gmail.com>
2023-03-17 10:11:44 +0530

    update delete room

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -35,6 +35,26 @@ router.post('/addroom',async(req,res) =>{
     }
 })
 
+router.delete('/deleterooms/:id', async (req,res) => {
+    try{
+        const rooms = await Room.findByIdAndDelete(
+            req.params.id
+        )
+        res.send(rooms)
+    }catch(error) { 
+        console.error(error);
+        res.status(500).json({ message: 'Error deleting room', error });    }
+  });
 
+// router.put('/updaterooms/:id', async (req,res) => {
+//     try{
+//         const rooms = await Room.findByIdAndUpdate(
+//             req.params.id,req.body,{ new: true }
+//         )
+//         res.send(rooms)
+//     }catch(error) { 
+//         console.error(error);
+//         res.status(500).json({ message: 'Error updating room', error });    }
+//   });
 
 module.exports = router
